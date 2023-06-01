ffe1ef8306f278df6665984639f4112b500efc90
obetdelacruz <pazmichaelandrew70@gmail.com>
2023-06-01 20:06:21 +0800

    Added dist folder

--- controllers/personController.js
+++ controllers/personController.js
@@ -1,52 +1,59 @@
 import Person from "../model/Person.js";
 
-async function getPersons(req, res) {
-  Person.find({}).then((persons) => {
-    res.status(200).json(persons);
-  });
+async function getPersons(_, res) {
+  const persons = await Person.find({});
+
+  return res.json(persons);
 }
 
 async function getPerson(req, res) {
   const id = req.params.id;
   const person = await Person.findById(id);
+
   return res.json(person);
 }
 
 async function createPerson(req, res) {
   const { name, number } = req.body;
+
   const person = new Person({
     name,
     number,
   });
 
-  const savePerson = await person.save();
+  const savedPerson = await person.save();
 
-  return res.status(201).json(savePerson);
-}
-
-async function deletePerson(req, res) {
-  const id = req.params.id;
-  await Person.findByIdAndDelete(id);
-  res.status(204).end();
+  return res.status(201).json(savedPerson);
 }
 
 async function updatePerson(req, res) {
   const id = req.params.id;
   const { name, number } = req.body;
+
   const person = {
     name,
     number,
   };
 
-  const updatePerson = await Person.findByIdAndUpdate(id, person);
+  const updatedPerson = await Person.findByIdAndUpdate(id, person, {
+    new: true,
+  });
 
-  res.json(updatePerson);
+  res.json(updatedPerson);
+}
+
+async function deletePerson(req, res) {
+  const id = req.params.id;
+
+  await Person.findByIdAndDelete(id);
+
+  res.status(204).end();
 }
 
 export default {
   getPersons,
   getPerson,
   createPerson,
-  deletePerson,
   updatePerson,
+  deletePerson,
 };
--- routes/personRouter.js
+++ routes/personRouter.js
@@ -4,5 +4,9 @@ import personController from "../controllers/personController.js";
 const personRouter = express.Router();
 
 personRouter.get("/", personController.getPersons);
+personRouter.get("/:id", personController.getPerson);
+personRouter.post("/", personController.createPerson);
+personRouter.put("/:id", personController.updatePerson);
+personRouter.delete("/:id", personController.deletePerson);
 
 export default personRouter;
